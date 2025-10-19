1d766d488eb899caa22254c44921d4a8f7bce126
monim-es <75100525+monim-es@users.noreply.github.com>
2025-10-19 01:23:12 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    base: process.env.VITE_BASE_PATH || "/book-library"
-  ],
+  plugins: [react()],
+  base: process.env.VITE_BASE_PATH || '/'
 })
